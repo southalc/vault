685424b0a578a8eb1d75ba1a270afcc9bbc600af
dmaes <dieter.maes@dmaes.be>
2023-08-09 16:56:59 +0200

    fix approle-agent vault-token.service
    Removed `enable` and `active` parameters.
    This is a oneshot service that's triggered by the corresponding .path,
    puppet must not start it.
    Setting `active` will cause puppet to start the service every run,
    since it's normal state will be `inactive`.

--- manifests/approle_agent.pp
+++ manifests/approle_agent.pp
@@ -130,8 +130,6 @@ define vault_secrets::approle_agent (
     }
 
     systemd::unit_file { "${title}-vault-token.service":
-      enable  => true,
-      active  => true,
       content => epp('vault_secrets/token_service.epp', {
           owner     => $owner,
           sink_file => $sink_file,
